541d51857a539c98452de208e4ca7e2b20388d29
weiyafei <2553241022@qq.com>
2020-05-25 12:00:16 +0800

    修正环境

--- src/plugins/navigation/components/Navigation.js
+++ src/plugins/navigation/components/Navigation.js
@@ -1,62 +1,60 @@
-import Routes from '../routes'
-import { getKey, matches } from '../utils'
+import Routes from "../routes";
+import { getKey, matches } from "../utils";
 
 export default (keyName) => {
   return {
-    name: 'navigation',
+    name: "navigation",
     abstract: true,
     props: {},
     data: () => ({
-      routes: Routes
+      routes: Routes,
     }),
     computed: {},
     watch: {
       routes(val) {
         for (const key in this.cache) {
           if (!matches(val, key)) {
-            const vnode = this.cache[key]
-            vnode && vnode.componentInstance.$destroy()
-            delete this.cache[key]
+            const vnode = this.cache[key];
+            vnode && vnode.componentInstance.$destroy();
+            delete this.cache[key];
           }
         }
       },
     },
     created() {
-      this.cache = {}
+      this.cache = {};
     },
     destroyed() {
       for (const key in this.cache) {
-        const vnode = this.cache[key]
-        vnode && vnode.componentInstance.$destroy()
+        const vnode = this.cache[key];
+        vnode && vnode.componentInstance.$destroy();
       }
     },
     render() {
-      const vnode = this.$slots.default ? this.$slots.default[0] : null
+      const vnode = this.$slots.default ? this.$slots.default[0] : null;
       if (vnode) {
-        vnode.key = vnode.key || (vnode.isComment
-          ? 'comment'
-          : vnode.tag)
-        // prevent vue-router reuse component
-        const key = getKey(this.$route, keyName)
+        vnode.key = vnode.key || (vnode.isComment ? "comment" : vnode.tag);
+        // 防止vue-router重用组件
+        const key = getKey(this.$route, keyName);
         if (vnode.key.indexOf(key) === -1) {
-          vnode.key = `__navigation-${key}-${vnode.key}`
+          vnode.key = `__navigation-${key}-${vnode.key}`;
         }
         if (this.cache[key]) {
           if (vnode.key === this.cache[key].key) {
-            // restore vnode from cache
-            vnode.componentInstance = this.cache[key].componentInstance
+            // 从缓存还原vnode
+            vnode.componentInstance = this.cache[key].componentInstance;
           } else {
-            // replace vnode to cache
-            this.cache[key].componentInstance.$destroy()
-            this.cache[key] = vnode
+            // 替换vnode缓存
+            this.cache[key].componentInstance.$destroy();
+            this.cache[key] = vnode;
           }
         } else {
-          // cache new vnode
-          this.cache[key] = vnode
+          // 缓存新的vnode
+          this.cache[key] = vnode;
         }
-        vnode.data.keepAlive = true
+        vnode.data.keepAlive = true;
       }
-      return vnode
-    }
-  }
-}
+      return vnode;
+    },
+  };
+};
