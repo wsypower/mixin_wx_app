8bbfbf4da85a433103732f458c6f14edbe59c5a8
nancy12138 <mxj198701@126.com>
2020-07-16 11:29:27 +0800

    进度查询中未提交的订单进入详情页应该是申办页

--- public/config.js
+++ public/config.js
@@ -1,9 +1,9 @@
 const URL_CONFIG = {
-    baseURL: '/dogapi', //本地环境
-    // baseURL: 'http://192.168.71.33:8999/dogapi', //测试环境
+    // baseURL: '/dogapi', //本地环境
+    baseURL: 'http://192.168.71.33:8999/dogapi', //测试环境
     // baseURL: 'http://220.191.224.215/dogapi', //正式环境
-    // fileUrl: 'http://220.191.224.215/dogfile/file/uploadFiles/' //正式环境附件服务器地址
     fileUrl: 'http://192.168.71.33:50000/file/file/uploadFiles/', //测试环境附件服务器地址
-    //csgjUrl: 'http://220.191.224.215/csgj/#/home', //正式环境
+    // fileUrl: 'http://220.191.224.215/dogfile/file/uploadFiles/' //正式环境附件服务器地址
     csgjUrl: 'http://192.168.71.33:9012/csgj/#/home' //测试环境
+    //csgjUrl: 'http://220.191.224.215/csgj/#/home', //正式环境
 }
