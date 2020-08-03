f91016cab1aff44e762942edac58f171db00cc48
nancy12138 <mxj198701@126.com>
2020-08-03 09:46:18 +0800

    ios系统性能优化

--- src/main.js
+++ src/main.js
@@ -26,8 +26,25 @@ Vue.use(animated)
 import vueFiltersDate from '@/filters/moment/main'
 Vue.use(vueFiltersDate)
 
-// import FastClick from "fastclick";
-// FastClick.attach(document.body);
+import FastClick from "fastclick";
+
+if ('addEventListener' in document) {
+  document.addEventListener('DOMContentLoaded', function() {
+    if (FastClick) {
+      FastClick.attach(document.body);
+    }
+  }, false);
+}
+FastClick.prototype.focus = function (targetElement) {
+  let length;
+  if (targetElement.setSelectionRange && targetElement.type.indexOf('date') !== 0 && targetElement.type !== 'time' && targetElement.type !== 'month') {
+    length = targetElement.value.length;
+    targetElement.focus();
+    targetElement.setSelectionRange(length, length);
+  } else {
+    targetElement.focus();
+  }
+};
 
 //
 // ─── PLUGINS ────────────────────────────────────────────────────────────────────
